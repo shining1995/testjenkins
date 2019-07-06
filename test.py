05eb1f90cc4163d2a5de4c196588e2631401c954
shining1995 <sihaining1995@gmail.com>
2019-07-06 12:35:37 +0800

    new

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-pipeline {
-    agent { docker 'python:3.5.1' }
-    stages {
-        stage('build') {
-            steps {
-                sh 'python --version'
-            }
-        }
-    }
-}
+print('jenkins success')
